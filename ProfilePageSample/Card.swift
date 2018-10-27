6ec14acfd1d7d06dae3a31e08cdeab40b5635feb
Denis <denis.melenevsky@gmail.com>
2018-10-27 17:59:36 +0300

    Card model.

---
+++ ProfilePageSample/Card.swift
@@ -0,0 +1,12 @@
+//
+//  Card.swift
+//  ProfilePageSample
+//
+//  Created by Denis on 10/27/18.
+//  Copyright © 2018 Denis. All rights reserved.
+//
+
+class Card : Decodable {
+	let imageUrl: String
+	let isDefault: Bool
+}
