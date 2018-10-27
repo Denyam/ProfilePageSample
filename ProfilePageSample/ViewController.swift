533fbefbdfe1ff7a956c73587845dd76b4f312d7
Denis <denis.melenevsky@gmail.com>
2018-10-28 01:08:49 +0300

    ViewController: collection view added, filling profile

--- ProfilePageSample/ViewController.swift
+++ ProfilePageSample/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 	
@@ -23,13 +24,23 @@ class ViewController: UIViewController {
 	private let buttonsToCardsOffset = 54.0
 	private let cardsToLogoOffset = 96.0
 	private let betweenButtons = 37.0
+	private let cardWidth = 255.0
+	private let cardHeight = 161.0
+	private let cardsAreaHeight = 228.0
+	private let cardSpacing = 25.0
+	private let cardVerticaloffset = 33.0
+
+	private let cardCellIdentifier = "CardCellIdentifier"
 
 	private var nameLabel: UILabel!
 	private var cityLabel: UILabel!
 	private var settingsButton: UIButton!
 	private var pictureImageView: UIImageView!
+	private var cardsCollectionView: UICollectionView!
 	
 	private lazy var viewModel = ProfilePageViewModel()
+	
+	private var defaultCellSelected = false
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -39,12 +50,37 @@ class ViewController: UIViewController {
 		_ = viewModel.observeProfile()
 			.takeUntil(self.rx.deallocated)
 			.subscribe(onNext: {[weak self] profile in
-			self?.fillProfile()
+			self?.fillProfile(profile)
 		})
 		_ = viewModel.observeAvatarImage()
 			.takeUntil(self.rx.deallocated)
 			.subscribe(onNext: {[weak self] image in
 				self?.pictureImageView.image = image
+			})
+		
+		_ = viewModel.observeCards()
+			.share(replay: 1)
+			.takeUntil(self.rx.deallocated)
+			.bind(to: cardsCollectionView.rx.items(cellIdentifier: cardCellIdentifier, cellType: CardCollectionViewCell.self)) { [weak self] index, card, cell in
+				cell.setImage(image: card.image)
+				if let defaultSelected = self?.defaultCellSelected, !defaultSelected {
+					if card.isDefault {
+						self?.defaultCellSelected = true
+						self?.cardsCollectionView.selectItem(at: IndexPath(item: index, section: 0), animated: true, scrollPosition: UICollectionViewScrollPosition.centeredHorizontally)
+					}
+				}
+		}
+		
+		_ = viewModel.cardImages()
+			.takeUntil(self.rx.deallocated)
+			.subscribe(onNext: {[weak self] card in
+				print((self?.viewModel.cards)!)
+				if let index = self?.viewModel.cards?.index(where: { $0 === card }) {
+					let indexPath = IndexPath(item: index, section: 0)
+					if let cell = self?.cardsCollectionView.cellForItem(at: indexPath) as? CardCollectionViewCell {
+						cell.setImage(image: card.image)
+					}
+				}
 			})
 	}
 
@@ -91,9 +127,17 @@ class ViewController: UIViewController {
 		let logoImageView = UIImageView(image: UIImage(named: "openpayBrandmarkPositiveRgb"))
 		logoImageView.setContentHuggingPriority(.required, for: .vertical)
 		
-		let cardsView = UIView()
+		let cardsLayout = UICollectionViewFlowLayout()
+		cardsLayout.scrollDirection = UICollectionViewScrollDirection.horizontal
+		cardsLayout.itemSize = CGSize(width: cardWidth, height: cardHeight)
+		cardsLayout.minimumLineSpacing = CGFloat(cardSpacing)
+		cardsLayout.sectionInset = UIEdgeInsets(top: CGFloat(cardVerticaloffset), left: 0, bottom: CGFloat(cardVerticaloffset), right: 0)
 		
-		let viewsDictionary: [String: UIView] = ["nameLabel": nameLabel, "cityLabel": cityLabel, "pictureImageView": pictureImageView, "settingsButton": settingsButton, "logoImageView": logoImageView, "cardsView": cardsView, "mobileButton": mobileButton,]
+		cardsCollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: cardsLayout)
+		cardsCollectionView.backgroundColor = UIColor(red: 225.0 / 255, green: 229.0 / 255, blue: 243.0 / 255, alpha: 1)
+		cardsCollectionView.register(CardCollectionViewCell.self, forCellWithReuseIdentifier: cardCellIdentifier)
+		
+		let viewsDictionary: [String: UIView] = ["nameLabel": nameLabel, "cityLabel": cityLabel, "pictureImageView": pictureImageView, "settingsButton": settingsButton, "logoImageView": logoImageView, "cardsCollectionView": cardsCollectionView, "mobileButton": mobileButton,]
 		for (_, view) in viewsDictionary {
 			view.translatesAutoresizingMaskIntoConstraints = false
 			self.view.addSubview(view)
@@ -103,9 +147,11 @@ class ViewController: UIViewController {
 		self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|-\(leadingOffset)-[cityLabel]", metrics: nil, views: viewsDictionary))
 		self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|-\(leadingOffset)-[settingsButton(==\(settinsButtonDimension))]-\(betweenButtons)-[mobileButton]", metrics: nil, views: viewsDictionary))
 		self.view.addConstraint(NSLayoutConstraint(item: logoImageView, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0))
-		self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[cardsView]|", metrics: nil, views: viewsDictionary))
+		self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[cardsCollectionView]|", metrics: nil, views: viewsDictionary))
 		
-		self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-\(labelsTopOffset)-[nameLabel]-\(labelsVerticalDistance)-[cityLabel]-\(labelsToButtons)-[settingsButton(==\(settinsButtonDimension))]-\(buttonsToCardsOffset)-[cardsView]-\(cardsToLogoOffset)-[logoImageView]-\(logoBottomOffset)-|", metrics: nil, views: viewsDictionary))
+		self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-\(labelsTopOffset)-[nameLabel]-\(labelsVerticalDistance)-[cityLabel]-\(labelsToButtons)-[settingsButton(==\(settinsButtonDimension))]-\(buttonsToCardsOffset)-[cardsCollectionView(==\(cardsAreaHeight))]", metrics: nil, views: viewsDictionary))
+		self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[logoImageView]-\(logoBottomOffset)-|", metrics: nil, views: viewsDictionary))
+
 		self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-\(pictureTopOffset)-[pictureImageView(==\(pictureDimension))]", metrics: nil, views: viewsDictionary))
 		self.view.addConstraint(NSLayoutConstraint(item: settingsButton, attribute: .centerY, relatedBy: .equal, toItem: mobileButton, attribute: .centerY, multiplier: 1, constant: 0))
 	}
@@ -114,11 +160,9 @@ class ViewController: UIViewController {
 		self.present(SettingsViewController(), animated: true, completion: nil)
 	}
 	
-	func fillProfile() {
-		if let profile = self.viewModel.profile {
-			self.nameLabel.text = "\(profile.firstName) \(profile.lastName)"
-			self.cityLabel.text = "\(profile.city), \(profile.country)"
-		}
+	func fillProfile(_ profile: Profile) {
+		self.nameLabel.text = "\(profile.firstName) \(profile.lastName)"
+		self.cityLabel.text = "\(profile.city), \(profile.country)"
 	}
 }
 
