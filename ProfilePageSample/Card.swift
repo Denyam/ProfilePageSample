0a6038e2f663ec4ab83eaf65e0a9a64f5399383c
Denis <denis.melenevsky@gmail.com>
2018-10-28 01:08:01 +0300

    Card: image property.

--- ProfilePageSample/Card.swift
+++ ProfilePageSample/Card.swift
@@ -6,7 +6,15 @@
 //  Copyright © 2018 Denis. All rights reserved.
 //
 
+import UIKit
+
 class Card : Decodable {
 	let imageUrl: String
 	let isDefault: Bool
+	var image: UIImage? = nil
+	
+	enum CodingKeys: String, CodingKey {
+		case imageUrl
+		case isDefault
+	}
 }
